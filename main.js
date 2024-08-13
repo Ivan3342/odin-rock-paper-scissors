be8cf96f41f97a6cb13d862ac18be5f80fd61002
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2024-08-13 14:57:46 +0200

    Fixed some more bugs, game works TODO: fix bugs on when game ends

--- main.js
+++ main.js
@@ -5,25 +5,32 @@ const winnerbox = document.querySelector(".winnertxt");
 const playerScoreBox = document.querySelector(".playerscore");
 const computerScoreBox = document.querySelector(".computerscore");
 
-
 let playerChoice;
 let playerScore = 0;
 
 let computerChoice;
 let computerScore = 0;
 
+
+
+
 function playRound(playerChoice) {
-    let computerChoice = getComputerChoice();
-    const winner = getWinner(playerChoice, computerChoice);
-    if(winner === "Player") {
-        playerScore++;
+    if (playerScore >= 5 || computerScore >= 5) {
+        stop;
     }
-    else if (winner === "Computer") {
-        computerScore++;
+    else {
+        let computerChoice = getComputerChoice();
+        const winner = getWinner(playerChoice, computerChoice);
+        if(winner === "Player") {
+            playerScore++;
+            playerScoreBox.textContent = "Your Score: " + playerScore;
+        }
+        else if (winner === "Computer") {
+            computerScore++;
+            computerScoreBox.textContent = "Computer Score: " + computerScore;
+        }
+        winnerbox.textContent = winner;
     }
-    winnerbox.textContent = winner;
-    playerScoreBox.textContent = "Your Score: " + playerScore;
-    computerScoreBox.textContent = "Computer Score: " + computerScore;
 }
 
 function startGame() {
@@ -42,21 +49,36 @@ function getComputerChoice() {
     const possibleChoices = ["rock", "paper", "scissors"];
     computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
     computerbox.textContent = computerChoice;
+    return computerChoice;
 }
 
-function getWinner(humanSelection, computerSelection) {
-    if (humanSelection == "rock" && computerSelection == "scissors" || humanSelection == "scissors" && computerSelection == "paper" || humanSelection == "paper" && computerSelection == "rock") {
+function getWinner(choice1, choice2) {
+    if (
+    (choice1 == "rock" && choice2 == "scissors") ||
+    (choice1 === "scissors" && choice2 === "paper") ||
+    (choice1 === "paper" && choice2 === "rock")    
+    ) {
         return "Player";
-    } 
-    else if (humanSelection == "rock" && computerSelection == "paper" || humanSelection == "paper" && computerSelection == "scissors" || humanSelection == "paper" && computerSelection == "rock") {
-        return "Computer";
     }
-    else {
+    else if (choice1 == choice2) {
         return "Draw";
     }
+    else {
+        return "Computer";
+    }
+    
 }
-
+startbtn.style.visibility = "hidden";
+startGame();
 startbtn.addEventListener("click", function() {
-    startGame();
-    startbtn.textContent = "RESET";
-})
+    startbtn.textContent = "PLAY AGAIN?";
+    playerScore = 0;
+    playerScoreBox.textContent = "Your Score: 0";
+    playerbox.textContent = "";
+    computerScore = 0;
+    computerScoreBox.textContent = "Computer Score: 0";
+    computerbox.textContent = "";
+    winnerbox.textContent = "";
+}
+)
+startbtn.style.visibility = "shown";
